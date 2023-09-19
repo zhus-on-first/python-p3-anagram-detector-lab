a63b3fc3137912c71c9e72d6c4ac6fbd76655c76
Jia-Jia Zhu <135554427+zhus-on-first@users.noreply.github.com>
2023-09-19 09:50:34 -0700

    lab complete with various solutions

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,40 @@
-# your code goes here!
+# anagram class
+# init with a word 
+# def match
+# if anagram return matches
+# else return none
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, potential_anagrams):
+     
+        sorted_word = sorted(self.word)
+
+        # matches = []
+        # for potential_anagram in potential_anagrams:
+        #     if sorted(potential_anagram) == sorted_word:
+        #         matches.append(potential_anagram)
+
+        matches = [potential_anagram for potential_anagram in potential_anagrams 
+                   if sorted(potential_anagram) == sorted_word ]
+        return matches
+        
+# Anagram(initial_word, check_words):
+#     listen = Anagram(initial_word)
+#     listen.match([check_words])
+
+# def Anagram(initial_word, check_words):
+#     possible_anagrams = []
+#     for check_word in check_words:
+#         if sorted(check_word) == sorted(initial_word):
+#             possible_anagrams.append(check_word)
+#         else:
+#             return []
+        
+#         return possible_anagrams
+
+# anagram if composed of same letters
+#   [letter for letter in "hello"]
+#   sorted([l, i, s, t, e, n]) == sorted([e, n, l, i, s, t, s])
